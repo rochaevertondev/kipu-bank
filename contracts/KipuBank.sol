8ed58ff76f46704cbdb31aa57c7c5f364244b8d1
RochaEverton <rochaevertondev@gmail.com>
2025-09-14 20:03:43 -0300

    Add error InvalidValue and MaxBankCapReached for validations in the function deposit

--- contracts/KipuBank.sol
+++ contracts/KipuBank.sol
@@ -3,65 +3,89 @@
 pragma solidity ^0.8.20;
 
 contract KipuBank {
-    
     uint256 public immutable MaxBankCap;
     uint256 public immutable LimitMaxPerWithdraw;
     address private immutable ownerBank;
-    
+
     uint256 private KipuBankBalance;
     uint256 private countDeposits;
     uint256 private countWithdrawals;
 
     mapping(address => uint256) private _balances;
-    
+
     error NotOwnerBank(address caller);
     error NotAccountOwner(address caller);
-    
+    error InvalidValue(uint256 value);
+    error MaxBankCapReached(uint256 value);
+
     constructor(uint256 _MaxBankCap, uint256 _LimitMaxPerWithdraw) {
         MaxBankCap = _MaxBankCap;
         LimitMaxPerWithdraw = _LimitMaxPerWithdraw;
         ownerBank = msg.sender;
-  
     }
 
     function deposit() external payable {
+        if (msg.value <= 0 || msg.value > LimitMaxPerWithdraw) {
+            revert InvalidValue(msg.value);
+        }
+        if (KipuBankBalance + msg.value > MaxBankCap) {
+            revert MaxBankCapReached(msg.value);
+        }
         _balances[msg.sender] += msg.value;
         KipuBankBalance += msg.value;
+        countDeposits++;
     }
 
     function withdraw() external payable {
         _balances[msg.sender] -= msg.value;
         KipuBankBalance -= msg.value;
+        countWithdrawals++;
     }
 
-    modifier onlyOwnerBank(){
-        if (msg.sender != ownerBank){
+    modifier onlyOwnerBank() {
+        if (msg.sender != ownerBank) {
             revert NotOwnerBank(msg.sender);
         }
         _;
     }
 
-    function currentBalance() external view onlyOwnerBank returns (uint256 current) {
+    function currentBalance()
+        external
+        view
+        onlyOwnerBank
+        returns (uint256 current)
+    {
         return KipuBankBalance;
     }
 
-    function getDeposits() external view onlyOwnerBank returns (uint256 current) {
+    function getDeposits()
+        external
+        view
+        onlyOwnerBank
+        returns (uint256 current)
+    {
         return countDeposits;
     }
 
-    function getWithdrawals() external view onlyOwnerBank returns (uint256 current) {
+    function getWithdrawals()
+        external
+        view
+        onlyOwnerBank
+        returns (uint256 current)
+    {
         return countWithdrawals;
     }
 
-    modifier onlyAccountOwner(address account){
-        if (msg.sender != account && msg.sender != ownerBank){
+    modifier onlyAccountOwner(address account) {
+        if (msg.sender != account && msg.sender != ownerBank) {
             revert NotAccountOwner(msg.sender);
         }
         _;
     }
 
-    function getBalance(address account) external view onlyAccountOwner(account) returns (uint256) {
+    function getBalance(
+        address account
+    ) external view onlyAccountOwner(account) returns (uint256) {
         return _balances[account];
     }
-
 }
